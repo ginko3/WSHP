990c4c7ca5ccfb45be29e4802134de0b562df476
Max Cohen <max.cohen@gmx.com>
2018-11-11 19:22:50 +0100

    added pathlib to solve python3 compatibility

--- parsing_network/inference.py
+++ parsing_network/inference.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import os
 import sys
 import time
+from pathlib import Path
 
 from PIL import Image
 
@@ -21,14 +22,14 @@ from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, prepa
 import pdb
 
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
-    
+
 NUM_CLASSES = 7
 DATA_LIST = './dataset/dance.txt'
 SAVE_DIR = './output/'
 
 def get_arguments():
     """Parse all the arguments provided from the CLI.
-    
+
     Returns:
       A list of parsed arguments.
     """
@@ -47,12 +48,12 @@ def get_arguments():
 
 def load(saver, sess, ckpt_path):
     '''Load trained weights.
-    
+
     Args:
       saver: TensorFlow saver object.
       sess: TensorFlow session.
       ckpt_path: path to checkpoint file with parameters.
-    ''' 
+    '''
     saver.restore(sess, ckpt_path)
     print("Restored model parameters from {}".format(ckpt_path))
 
@@ -68,7 +69,7 @@ def main():
     num_steps = file_len(args.data_list)
     # Create queue coordinator.
     coord = tf.train.Coordinator()
-    
+
     # Load reader.
     with tf.name_scope("create_inputs"):
         reader = ImageReader(
@@ -83,7 +84,7 @@ def main():
         image, label = reader.image, reader.label
         title = reader.queue[0]
     image_batch, label_batch = tf.expand_dims(image, dim=0), tf.expand_dims(label, dim=0) # Add one batch dimension.
-    
+
     # Create network.
     net = DeepLabResNetModel({'data': image_batch}, is_training=False, num_classes=args.num_classes)
 
@@ -96,19 +97,19 @@ def main():
     raw_output_up = tf.argmax(raw_output_up, dimension=3)
     pred = tf.expand_dims(raw_output_up, dim=3)
 
-    
-    # Set up TF session and initialize variables. 
+
+    # Set up TF session and initialize variables.
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
     init = tf.global_variables_initializer()
-    
+
     sess.run(init)
-    
+
     # Load weights.
     loader = tf.train.Saver(var_list=restore_var)
     load(loader, sess, args.model_weights)
-    
+
     # Start queue threads.
     threads = tf.train.start_queue_runners(coord=coord, sess=sess)
 
@@ -121,16 +122,17 @@ def main():
         msk = decode_labels(preds, num_classes=args.num_classes)
         im = Image.fromarray(msk[0])
         img_o = Image.open(jpg_path)
-        jpg_path = jpg_path.split('/')[-1].split('.')[0]
+        jpg_path = Path(str(jpg_path)).stem
+        print(type(jpg_path), jpg_path)
         img = np.array(im)*0.9 + np.array(img_o)*0.7
         img[img>255] = 255
         img = Image.fromarray(np.uint8(img))
         img.save(args.save_dir + jpg_path + '.png')
         print('Image processed {}.png'.format(jpg_path))
-    
+
     total_time = time.time() - start_time
     print('The output files have been saved to {}'.format(args.save_dir))
     print('It took {} sec on each image.'.format(total_time/num_steps))
-    
+
 if __name__ == '__main__':
     main()
